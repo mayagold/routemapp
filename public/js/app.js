d14a900f61527bf0a181d095e7bda7ae5401ea7d
mayagold <maya.gold.1991@gmail.com>
2017-08-17 16:18:21 -0700

    basic structure of the app is set

--- public/js/app.js
+++ public/js/app.js
@@ -5,5 +5,5 @@
 const app = angular.module("routemapp", []);
 
 app.controller('testController', ['$http', function($http){
-  this.test = 'Motorcycle Route App'
+  this.test = 'Motorcycle Route App: This is the index page and Angular is working'
 }])
