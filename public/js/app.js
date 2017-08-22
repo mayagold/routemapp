84a0d6bf3f22a1cad4914bd99617bcef1b938913
Samantha Ramos <samantharamos@samanthas-air.attlocal.net>
2017-08-21 19:46:57 -0500

    sessions fixed --- function works but needs tweeking and needs to be implemented on page

--- public/js/app.js
+++ public/js/app.js
@@ -10,6 +10,47 @@ app.controller('routeController', ['$http', function($http){
   this.test = 'Motorcycle Route App: This is the index page and Angular is working'
   this.routes = "";
 
+this.checkRegister = function(username, password){
+    $http({
+      method: 'POST',
+      url: '/session/registration',
+      data: {
+        username: username,
+        password: password
+      }
+    }).then(
+      function(response){
+        console.log(response);
+        // controller.User.create();
+      },
+      function(error){
+
+      }
+    );
+
+}
+ // this.checkRegister('test', 'Samantha9');
+
+this.checkAuth = function(username, password){
+    $http({
+      method: 'POST',
+      url: '/session/login',
+      data: {
+        username: username,
+        password: password
+      }
+    }).then(
+      function(response){
+        // console.log('---------- this is the login route response', response, '----------');
+        controller.username = response.data
+      },
+      function(error){
+        console.log(error);
+      }
+    );
+}
+ this.checkAuth('test', 'Samantha9');
+
 this.getRoutes = function(){
     $http({
       method: 'GET',
@@ -17,7 +58,7 @@ this.getRoutes = function(){
     }).then(
       function(response){
         console.log(controller);
-        console.log('--------------', response.data, '--------------');
+        // console.log('--------------', response.data, '--------------');
         controller.routes = response.data
       },
       function(error){
@@ -45,13 +86,15 @@ this.createRoute = function(){
     );
 }
 this.editRoute = function(routes){
+  console.log(this.updateRoute);
+  console.log(req.session);
   $http({
     method: 'PUT',
     url: '/routes/' + routes._id,
     data: {
       gpxFile: this.gpxFile,
-      comments: this.comments
-      // userData: this.userData  //not sure about this .. if we use, add comma above
+      comments: this.updateRoute,
+      userData: this.userData  //not sure about this .. if we use, add comma above
       // req.session.logged = true ...statement
     }
   }).then(
