48a6fac140a81e33c70ffa5b09ee39647a618004
Linwood Lassiter <lin1ood@embarqmail.comx>
2017-08-23 08:37:47 -0400

    added some more routes and updated that path for their location

--- controllers/seedController.js
+++ controllers/seedController.js
@@ -14,7 +14,7 @@ const Routes = require('../models/routes.js');
 ////////////////////////////////////////////////
 const newRoutes=[
   {
-    gpxFile: "../routes/'Burbon Trail'.gpx",
+    gpxFile: "/routes/'BurbonTrail'.gpx",
     comments: "Scenic tour to several of the Burbon Distillers.",
     userData: {
       username: "seedRun",
@@ -23,7 +23,7 @@ const newRoutes=[
     }
   },
   {
-    gpxFile: "../routes/'Dahlonega to Amicalola Falls State Park'.gpx",
+    gpxFile: "/routes/DahlonegaAmicalolaFalls.gpx",
     comments: "NE Georgia Scenic route between Dahlonega to Amicalola Falls Stat Park",
     userData: {
       username: "seedRun",
@@ -32,7 +32,7 @@ const newRoutes=[
     }
   },
   {
-    gpxFile: "../routes/'Hatteras LH - Bodie LH'.gpx",
+    gpxFile: "/routes/HatterasBodieLH.gpx",
     comments: "NC Outer Banks route from Bodie Lighthouse to Cape Lookout Lighthouse",
     userData: {
       username: "seedRun",
@@ -41,7 +41,7 @@ const newRoutes=[
     }
   },
   {
-    gpxFile: "../routes/'Helen Loop'.gpx",
+    gpxFile: "/routes/HelenGA.gpx",
     comments: "Scenic tour from wester NC to NE Georgia with the bonus of a visit of the Baverian themed town of Helen GA.",
     userData: {
       username: "seedRun",
@@ -50,7 +50,7 @@ const newRoutes=[
     }
   },
   {
-    gpxFile: "../routes/'Homestaed to KeyWest'.gpx",
+    gpxFile: "/routes/Homestaed2KeyWest.gpx",
     comments: "South Florida tour between Homestaed and Key West.",
     userData: {
       username: "seedRun",
@@ -59,7 +59,7 @@ const newRoutes=[
     }
   },
   {
-    gpxFile: "../routes/NorthCarolinaTransportation.gpx",
+    gpxFile: "/routes/NorthCarolinaTransportation.gpx",
     comments: "Scenic route across NC piedmont and with way points set for visiting the 'Big Chair' in Thomasville, NC -- Daniel Boones Cave in Churchland, NC and NC  Transportation Museum in Spencer, NC.",
     userData: {
       username: "seedRun",
@@ -68,7 +68,7 @@ const newRoutes=[
     }
   },
   {
-    gpxFile: "../routes/'Pioneer Village to Casino Queen RV'.gpx",
+    gpxFile: "/routes/PioneerVillage2Arch.gpx",
     comments: "Route between the Pioneer Village in Minden, NE and the Gateway to the West in St. Louis, MO. ",
     userData: {
       username: "seedRun",
@@ -77,8 +77,35 @@ const newRoutes=[
     }
   },
   {
-    gpxFile: "../routes/Spearfish.gpx",
+    gpxFile: "/routes/Spearfish.gpx",
     comments: "Scenic route touring Spearfish Canyon and Custer SD.  A ride through the Northern Black Hills with vistas and views all along the way.",
+    userData: {
+      username: "seedRun",
+      password: "$2a$10$tT8Bl0Fzu2VY1apOadUKW.br8NkDAkfhm6CelvzjTCL8GWUpbHFfC",
+      Img: "Image"
+    }
+  },
+  {
+    gpxFile: "/routes/Waco2Austin.gpx",
+    comments: "Scenic route touring Texas Hill country between Waco and Austin TX.",
+    userData: {
+      username: "seedRun",
+      password: "$2a$10$tT8Bl0Fzu2VY1apOadUKW.br8NkDAkfhm6CelvzjTCL8GWUpbHFfC",
+      Img: "Image"
+    }
+  },
+  {
+    gpxFile: "/routes/Wildlifeloop-Needles-Rushmore-CrazyHorse.gpx",
+    comments: "Scenic route in the Black Hills including Needles Highway, Mt. Rushmore and Crazy Horse Monuments. Watch out for Buffalo.",
+    userData: {
+      username: "seedRun",
+      password: "$2a$10$tT8Bl0Fzu2VY1apOadUKW.br8NkDAkfhm6CelvzjTCL8GWUpbHFfC",
+      Img: "Image"
+    }
+  },
+  {
+    gpxFile: "/routes/WoodsideLk2Rock&RollHallofFame.gpx",
+    comments: "Route from south of Cleveland to the Rock & Roll Hall of Fame.",
     userData: {
       username: "seedRun",
       password: "$2a$10$tT8Bl0Fzu2VY1apOadUKW.br8NkDAkfhm6CelvzjTCL8GWUpbHFfC",
